f6e746985276c0db4c982bf8d1c8991189cde708
Daniel L <daniel.cl@yahoo.com>
2015-09-09 13:21:55 +0200

    Second commit.

---
+++ Assignment_1/Sensor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Sensor.h"
+
+int getNextData(){
+	// Implement me according to the Assignment 1 manual
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int value;
+	static const char filename[] = "ECG.txt";
+	FILE *file = fopen(filename,"r");
+	fscanf(file,"%i",&value)
+
+
+
+
+
+
+
+
+
+
+}
