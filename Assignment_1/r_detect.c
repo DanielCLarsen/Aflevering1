67359c7fa0dddd56de7af80d201ff13e8043aec4
Michael Romer Svendsen <s134277@student.dtu.dk>
2015-09-22 17:06:17 +0200

    Så virker det shit igen.. for nu..

---
+++ Assignment_1/r_detect.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sensor.h"
+
+void r_detecter(peakstruct *peakdata, int current_peak){
+int xn = peakdata->PEAKS[1][current_peak];
+
+if (peakdata->THRESHOLD1 < xn){
+
+	peakdata->SPKF = 0.125*xn+0.875*peakdata->SPKF;
+
+	peakdata->NPKF = 0.125*xn+0.875*peakdata->NPKF;
+
+	peakdata->THRESHOLD1 = peakdata->NPKF + 0.25*(peakdata->SPKF - peakdata->NPKF);
+
+	peakdata->THRESHOLD2 = peakdata->THRESHOLD1*0.5;
+
+
+}
+
+}
--- Assignment_1/sensor.h
+++ Assignment_1/sensor.h
@@ -1,3 +1,5 @@
+// #define DZ 33
+
 typedef struct{
 	int sensor_input;
 	FILE *file_p;
@@ -10,8 +12,21 @@ typedef struct{
 	int mw_data[33];
 	int ix;
 	int time_delay;
+	int p_xleft;
+	int p_xmid;
+	int p_xright;
+
 
 } datastruct;
+
+typedef struct{
+	int PEAKS[2][33];
+	int R_PEAKS[2][33];
+	int NPKF;
+	int SPKF;
+	int THRESHOLD1;
+	int THRESHOLD2;
+} peakstruct;
 
 
 int getNextData(FILE *file_p);
@@ -25,3 +40,7 @@ int de_filter(datastruct *dataset);
 int sq_filter(datastruct *dataset);
 
 int mw_filter(datastruct *dataset);
+
+int p_search(datastruct *dataset);
+
+void r_detecter(peakstruct *peakdata, int current_peak);
